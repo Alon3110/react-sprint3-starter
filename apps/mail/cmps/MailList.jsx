ed871043625f509f4884f97020413e63efbc98ca
Alon3110 <6800688@gmail.com>
2025-07-30 22:50:10 +0300

    Refactor Mail preview to Mail List

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -3,9 +3,21 @@ import { MailPreview } from "./MailPreview.jsx";
 export function MailList({ mails, onRead, setMailId }) {
 
     return (
-        <article>
-            <h1>{mails.to}</h1>
-            <MailPreview mails={mails} onRead={onRead} setMailId={setMailId}/>
+        <article className="mail-list">
+            <ul>
+                {mails.map(mail => (
+                    <li
+                        key={mail.id}
+                        className={`cell ${mail.isRead ? 'read' : ''}`}
+                        onClick={() => {
+                            setMailId(mail.id)
+                            onRead(mail.id)
+                        }}
+                    >
+                        <MailPreview mail={mail} />
+                    </li>
+                ))}
+            </ul>
         </article>
     )
 }
