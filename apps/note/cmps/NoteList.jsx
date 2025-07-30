b2e4f49effd6008c5487051ed5389b8cd63276eb
avishaigal <avishai.gal@gmail.com>
2025-07-30 21:04:04 +0300

    Add note dynamic comp rf code

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -3,12 +3,12 @@ import { NotePreview } from "../cmps/NotePreview.jsx"
 export function NoteList({ notes }) {
 
     if (!notes) return <div className="loader">Loading...</div>
-    return <section className="notes-container">
-        {notes.map(note => (
-            <div key={note.id}>
-                <NotePreview note={note} />
-            </div>
-        ))}
-        <button className="btn-add-note">+</button>
-    </section>
+    return (<section className="notes-container">
+            {notes.map(note => (
+                <div key={note.id}>
+                    <NotePreview note={note} />
+                </div>
+            ))}
+            {/* <button className="btn-add-note" onClick={toggleAddNote}>+</button> */}
+        </section>)
 }
