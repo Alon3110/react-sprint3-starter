054e2a259d10d197d8b64dc153710375220afbf2
Alon3110 <6800688@gmail.com>
2025-07-30 15:57:17 +0300

    Orgenize the project and render a list with css a bit

--- apps/mail/pages/MailIndex.jsx
+++ apps/mail/pages/MailIndex.jsx
@@ -1,5 +1,28 @@
+import { mailService } from "../services/mail.service.js"
+import { MailList } from "../cmps/MailList.jsx"
+
+const { useState, useEffect } = React
 
 export function MailIndex() {
-    return <section className="container">Mail app</section>
+    const [mails, setMails] = useState(null)
+
+    useEffect(() => {
+        loadMails()
+    }, [])
+
+    function loadMails() {
+        mailService.query()
+            .then(mails => setMails(mails))
+            .catch(err => console.log('err:', err))
+    }    
+
+    if (!mails) return <div>Loading...</div>
+
+    return (
+        <section className="container">
+            <h1>Mail app</h1>
+            {mails && <MailList mails={mails} />}
+        </section>
+    )
 }
 
