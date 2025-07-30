903b311c46473cd38ed04f02358ef52f3f2d814d
avishaigal <avishai.gal@gmail.com>
2025-07-30 16:21:35 +0300

    Create add-btn and basic style

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -1,14 +1,14 @@
 import { NotePreview } from "../cmps/NotePreview.jsx"
 
-
 export function NoteList({ notes }) {
 
     if (!notes) return <div className="loader">Loading...</div>
     return <section className="notes-container">
         {notes.map(note => (
-    <div key={note.id}>
-        <NotePreview note={note} />
-    </div>
-))}
+            <div key={note.id}>
+                <NotePreview note={note} />
+            </div>
+        ))}
+        <button className="btn-add-note">+</button>
     </section>
 }
