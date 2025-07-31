9f618bbfd4e0e7c8735a084aa35508bbf8e28147
Alon3110 <6800688@gmail.com>
2025-07-31 20:58:49 +0300

    Add mail styling to the project

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -2,13 +2,16 @@ const { useParams, useNavigate, Link } = ReactRouterDOM
 
 export function MailPreview({ mail, onRemove }) {
 
-        
     return (
         <div className="details">
-            <h2>{mail.from}</h2>
+            <h2>{mail.to}</h2>
             <h4>{mail.subject} <span>{mail.body}</span></h4>
             <p>{new Date(mail.sentAt).toLocaleString()}</p>
-            <button onClick={(ev) => { ev.stopPropagation(), onRemove(mail.id)}} className='close'>Remove<i class="fa-solid fa-trash-can"></i></button>
+            <button onClick={(ev) => {
+                ev.stopPropagation(),
+                onRemove(mail.id)
+            }}
+                className='close'>Remove<i class="fa-solid fa-trash-can"></i></button>                
         </div>
     )
 }
